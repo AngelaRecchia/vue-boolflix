8a0be3badf9ba05f943d0e41ac5a36ca8c84558d
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-12 20:00:22 +0200

    add flag, play

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="box" v-if="show.vote_count > 20">
-    <img
-      :src="'https://image.tmdb.org/t/p/w500' + show.poster_path"
-      :alt="show.title"
-    />
+    <div class="imgBox">
+      <img
+        class="poster"
+        :src="'https://image.tmdb.org/t/p/w500' + show.poster_path"
+        :alt="show.title"
+      />
+
+      <img
+        class="play"
+        src="https://img.icons8.com/ios-filled/50/000000/play-button-circled--v1.png"
+      />
+    </div>
 
     <div class="title">{{ show.title }}</div>
     <div class="orig-title">{{ show.original_title }}</div>
     <div class="vote">Rating: {{ show.vote_average }}</div>
-    <div class="lang">{{ show.original_language }}</div>
+    <div class="lang">
+      <img
+        v-if="show.original_language == 'en'"
+        src="https://www.countryflags.io/us/flat/64.png"
+        alt="en"
+      />
+      <img
+        v-else
+        :src="
+          'https://www.countryflags.io/' +
+          show.original_language +
+          '/flat/64.png'
+        "
+        :alt="show.original_language"
+      />
+    </div>
   </div>
 </template>
 
@@ -26,8 +49,9 @@ export default {
   background-color: rgba($color3, $alpha: 0.4);
   margin: 10px;
   color: black;
-  img {
+  .poster {
     width: 100%;
+    transition: filter 0.5s;
   }
   .title {
     font-weight: bold;
@@ -36,14 +60,31 @@ export default {
     font-style: italic;
     font-size: 14px;
   }
-  .lang {
-    text-transform: uppercase;
+  .lang img {
+    width: 20px;
   }
   .vote {
     font-size: 12px;
   }
-  &:hover img {
+  .imgBox {
+    position: relative;
+  }
+  .play {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    border-radius: 50%;
+    opacity: 0;
+    transition: opacity 0.5s;
+    cursor: pointer;
+  }
+  &:hover .poster {
     filter: brightness(50%);
+  }
+  &:hover .play {
+    opacity: 1;
   }
 }
 </style>
