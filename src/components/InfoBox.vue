14ccc83abb1c69fdc53929b61218429422db2e41
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-13 18:27:46 +0200

    add genres

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -1,14 +1,19 @@
 <template>
   <b-tooltip :target="this.$parent" triggers="hover" placement="right">
     <div class="infos">
-      <span class="title">{{ title }}</span>
+      <div class="title">{{ title }}</div>
       <div class="datas d-flex justify-content-between">
         <span><i class="fas fa-star"></i> {{ vote }}</span>
         <span>{{ duration }}</span>
         <span>{{ year.substring(0, 4) }}</span>
       </div>
-      <div class="overview">
-        <strong>Overview:</strong> {{ overview.substring(0, 200) }}...
+      <div class="overview">{{ overview.substring(0, 200) }}...</div>
+      <div class="genres">
+        <span>Genres: </span>
+        <span v-for="(genre, index) in genres" :key="index">
+          <span class="genre">{{ genre.name }}</span>
+          <span v-if="index < genres.length - 1">, </span>
+        </span>
       </div>
     </div>
   </b-tooltip>
@@ -26,6 +31,7 @@ export default {
       year: "",
       duration: "",
       overview: "",
+      genres: "",
     };
   },
   mounted() {
@@ -43,6 +49,7 @@ export default {
             this.title = result.data.title;
             this.year = result.data.release_date;
             this.duration = result.data.runtime + "min";
+            this.genres = result.data.genres;
           });
       } else {
         axios
@@ -57,6 +64,7 @@ export default {
             this.title = result.data.name;
             this.year = result.data.first_air_date;
             this.duration = result.data.episode_run_time + "min";
+            this.genres = result.data.genres;
           });
       }
     });
@@ -68,12 +76,26 @@ export default {
 @import "@/style/commons.scss";
 .infos {
   padding: 10px;
+  text-align: left;
 }
 .title {
   font-size: 15px;
   font-weight: 700;
+  text-align: center;
 }
-.datas {
-  margin: 10px 0;
+.datas,
+.overview,
+.genres {
+  margin-top: 10px;
+}
+.overview {
+  font-size: 12px;
+}
+.genres {
+  font-size: 13px;
+}
+.genre {
+  cursor: pointer;
+  color: $color3;
 }
 </style>
