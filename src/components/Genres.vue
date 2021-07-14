e60221fbbfc5bcb408b813434790d222104cda03
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-14 19:29:36 +0200

    checkbox wip

--- src/components/Genres.vue
+++ src/components/Genres.vue
@@ -7,18 +7,20 @@
       </span>
     </div>
 
-    <b-tooltip target="buttonG" triggers="click" placement="bottomleft">
+    <div id="showG">
       <b-form-group v-model="pickedG">
-        <b-form-checkbox-group id="pickG" v-model="pickedG">
+        <b-form-checkbox-group v-model="pickedG" id="listG">
           <b-form-checkbox
+            class="elemCheck"
             v-for="(genre, index) in options"
             :key="index"
             :value="genre"
-            >{{ genre.name }}</b-form-checkbox
-          >
+            @change="check(genre)"
+            ><span class="ms-1">{{ genre.name }}</span>
+          </b-form-checkbox>
         </b-form-checkbox-group>
       </b-form-group>
-    </b-tooltip>
+    </div>
   </div>
 </template>
 
@@ -28,24 +30,62 @@ export default {
   name: "PickGenres",
   data() {
     return {
-      pickedG: ["All"],
+      all: { id: 0, name: "All" },
+      pickedG: [],
       options: [],
     };
   },
   mounted() {
     this.$nextTick(function () {
-      const all = { id: 0, name: "All" };
+      this.pickedG.push(this.all);
+
       const listGMovies =
         "https://api.themoviedb.org/3/genre/movie/list?api_key=1dafa5cfbeee5c77b53b196c6bc8c45d";
 
       axios.get(listGMovies).then((res) => {
-        this.options = [all, ...res.data.genres];
+        this.options = [this.all, ...res.data.genres];
       });
     });
+  },
+  updated() {
+    if (this.pickedG.length == 0) this.pickedG.push(this.all);
+  },
+  methods: {
+    check(val) {
+      if (val.id != 0 && this.pickedG.includes(this.all)) {
+        this.pickedG.splice(
+          this.pickedG.findIndex((elem) => elem == this.all),
+          1
+        );
+      }
+      if (val.id == 0) this.pickedG = [this.all];
+      console.log(val.id);
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/style/commons.scss";
+#filterGenres {
+  margin: 20px 0;
+}
+#buttonG {
+  background-color: black;
+  border-radius: 5px;
+  padding: 10px 15px;
+  display: inline-block;
+}
+#listG {
+  width: 40vw;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  font-size: 13px;
+  padding: 15px;
+  background-color: black;
+  margin-top: 10px;
+  border-radius: 5px;
+}
 </style>
